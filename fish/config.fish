9eb6440721365601a615abf546e9f37269e9ae20
unbalancedparentheses <federico.carrone@gmail.com>
2017-09-08 00:11:31 -0300

    Move back to bspwm, add new wallpapers and add kiex and kerl to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,5 +4,10 @@ if [ (uname) = "OpenBSD" ]
    set -x PKG_PATH http://openbsd.cs.toronto.edu/pub/OpenBSD/snapshots/packages/amd64
 end
 
+set -x PATH $PATH ~/.mix ~/bin 
 set -x LC_ALL en_US.utf8
 set -x LANG en_US.utf8
+
+source $HOME/.kiex/elixirs/.elixir-1.3.4.env.fish
+source $HOME/erlang/19.3/activate.fish
+
