6d1e2e484633d37d1c57f64ac52a0c171cd75173
unbalancedparentheses <mail@fcarrone.com>
2019-08-19 13:18:56 -0300

    Add home bin

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,6 @@ if [ (uname) = "OpenBSD" ]
    set -x PKG_PATH http://openbsd.cs.toronto.edu/pub/OpenBSD/snapshots/packages/amd64
 end
 
-set -x PATH $PATH ~/.cargo/bin/
+set -x PATH $PATH ~/.cargo/bin/ ~/bin/discord/
 set -x LC_ALL en_US.utf8
 set -x LANG en_US.utf8
