2d55bdd8bd2de571f5219c31fe55574e7014ff8b
unbalancedparentheses <federico.carrone@gmail.com>
2022-01-08 21:07:52 -0300

    Update makefile and nix

--- nix/home.nix
+++ nix/home.nix
@@ -7,6 +7,7 @@
   home.username = "unbalanced";
   home.homeDirectory = "/home/unbalanced";
 
+  home.allowUnfree = true;
   home.packages = with pkgs; [
     exa
     mosh
@@ -24,7 +25,53 @@
     prettyping
     wget
     youtube-dl
+    lua
   ];
+
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+          viAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      # Appearance
+      vim-table-mode # vimscript
+      indentLine  # vimscript
+      indent-blankline-nvim
+      nvim-tree-lua
+      nvim-web-devicons
+      one-nvim
+
+      # Programming
+      vim-which-key          # vimscript
+      vim-nix                # vimscript
+      lspkind-nvim
+      nvim-treesitter
+      nvim-treesitter-refactor
+      nvim-treesitter-textobjects
+      nvim-lspconfig
+      nvim-compe
+      vim-vsnip
+      vim-vsnip-integ
+
+      # Text objects
+      tcomment_vim    # vimscript
+      vim-surround    # vimscript
+      vim-repeat      # vimscript
+      nvim-autopairs
+
+      # Git
+      vim-fugitive  # vimscript
+      vim-gitgutter # vimscript
+
+      # DAP
+      vimspector # vimscript
+
+      # Fuzzy Finder
+	#      telescope-nvim
+    ];
+  };
+
 
   programs.bat = {
     enable = true;
