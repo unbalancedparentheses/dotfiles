15860b1e80ee23d435bdaa3a15cb5687b30201a5
unbalancedparentheses <federico.carrone@gmail.com>
2022-01-09 10:27:04 -0300

    Update names and add files

--- openbsd/vm.sh
+++ openbsd/vm.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-if [ ! -f ./install70.iso ]; then
-	wget https://cdn.openbsd.org/pub/OpenBSD/7.0/amd64/install70.iso
+ISO=install70.iso
+URL=https://cdn.openbsd.org/pub/OpenBSD/7.0/amd64/$ISO
+
+if [ ! -f ./$ISO ]; then
+	wget $URL > $ISO
 fi
 
 if [ ! -f ./openbsd.img ]; then
 	qemu-img create openbsd.img 10G
+	qemu-system-x86_64 -boot d -cdrom ./$ISO -m 2048 -hda openbsd.img -enable-kvm -curses
 fi
 
-qemu-system-x86_64 -boot d -cdrom ./install70.iso -m 2048 -hda openbsd.img -enable-kvm -curses
+qemu-system-x86_64 -m 2048 -nic user -hda openbsd.img -enable-kvm -curses 
