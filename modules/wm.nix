1823093065bd1deef7d788263021cb47b2d8f8a1
Federico Carrone <mail@fcarrone.com>
2026-01-17 13:19:21 -0300

    AeroSpace: remove Ghostty workspace rule
    Allow terminal to open on any workspace

--- modules/wm.nix
+++ modules/wm.nix
@@ -108,10 +108,6 @@ in
     run = 'move-node-to-workspace 1'
 
     [[on-window-detected]]
-    if.app-id = 'com.mitchellh.ghostty'
-    run = 'move-node-to-workspace 2'
-
-    [[on-window-detected]]
     if.app-id = 'dev.zed.Zed'
     run = 'move-node-to-workspace 2'
 
