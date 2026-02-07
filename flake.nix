841cb6f1333ede18b870b4204a1efb02373aa46f
Federico Carrone <mail@fcarrone.com>
2026-02-07 20:32:06 -0300

    fix removing the tiling manager

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     # User configuration
-    username = "unbalancedparen";
+    username = "federicocarrone";
     gitName = "Federico Carrone";
     gitEmail = "mail@fcarrone.com";
 
